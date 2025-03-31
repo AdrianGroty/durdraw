9a6219ed74ed33ab3c40fae9bd894ddb220cdca9
Sam Foster <samfoster@gmail.com>
2025-03-31 16:31:56 -0500

    Changed default FPS from 8 to 12

--- durdraw/durdraw_options.py
+++ durdraw/durdraw_options.py
@@ -3,7 +3,7 @@ import json
 class Options():    # config, prefs, preferences, etc. Per movie. Separate from AppState options.
     """ Member variables are canvas X/Y size, Framerate, Video resolution, etc """
     def __init__(self, width=80, height=23):         # default options
-        self.framerate = 8.0
+        self.framerate = 12.0
         self.sizeX = width
         self.sizeY = height
         self.saveFileFormat = 7 # save file format version number
